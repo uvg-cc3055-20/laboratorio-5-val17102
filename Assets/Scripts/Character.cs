924322fd830cef171ad6cd91c02001ba51a8c80c
Alhvi Balcarcel <abalcarcel@gmail.com>
2018-02-14 01:52:28 -0600

    Se agregaron los assets y primera escena

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour {
+
+    public List<GameObject> prefabs;
+    Rigidbody2D rb2d;
+    SpriteRenderer sr;
+    Animator anim;
+    private float speed = 5f;
+    private float jumpForce = 250f;
+    private bool facingRight = true;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float move = Input.GetAxis("Horizontal");
+        if (move != 0) {
+            rb2d.transform.Translate(new Vector3(1, 0, 0) * move * speed * Time.deltaTime);
+            facingRight = move > 0;
+        }
+
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        sr.flipX = !facingRight;
+
+        if (Input.GetButtonDown("Jump")) {
+            rb2d.AddForce(Vector2.up*jumpForce);
+        }
+	}
+}
